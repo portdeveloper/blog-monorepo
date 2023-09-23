1325726332c3a7e1302b5b39f4a32ed93e3f0db4
port <108868128+portdeveloper@users.noreply.github.com>
2023-09-23 08:42:10 +0300

    Remove unnecessary route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const passport = require("passport");
 const authController = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/login", authController.login);
-router.get(
-  "/protected",
-  passport.authenticate("jwt", { session: false }),
-  authController.protected
-);
 
 module.exports = router;
