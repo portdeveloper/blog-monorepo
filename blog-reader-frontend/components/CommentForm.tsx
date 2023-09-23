02332f40111b3c737e0084d2ab9f39d198e74464
port <108868128+portdeveloper@users.noreply.github.com>
2023-09-23 20:08:28 +0300

    Add CommentForm

---
+++ blog-reader-frontend/components/CommentForm.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { FormEvent, useState } from "react";
+
+export default function CommentForm({ blogpostId }: { blogpostId: string }) {
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(
+        `http://localhost:5000/blogposts/${blogpostId}/comments`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, content }),
+        }
+      );
+      if (!res.ok) {
+        throw Error(`Error: ${res.statusText}`);
+      }
+      setName("");
+      setContent("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className="flex flex-col space-y-4 form-control mt-4">
+      <div className="flex gap-2 items-center">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your name"
+          className="input input-sm input-bordered"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <label htmlFor="comment">Comment</label>
+
+      <textarea
+        className="textarea textarea-bordered"
+        placeholder="Write a comment..."
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      ></textarea>
+      <button
+        type="submit"
+        className="btn btn-primary btn-xs"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
